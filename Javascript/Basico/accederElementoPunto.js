284aa0981d28f24d15e797baf0ddec2bfc1eac00
juanjo.figue97 <juanjo.figue97@gmail.com>
2022-09-20 16:57:09 -0500

    Obtener un elemento con punto

---
+++ Javascript/Basico/accederElementoPunto.js
@@ -0,0 +1,11 @@
+// Configuración
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+// Acceder a elemento obj.propiedad
+const hatValue = testObj.hat;      
+const shirtValue = testObj.shirt;    
+
