b962c6886c65a082e932c03dbd3c168e89b7d992
juanjo.figue97 <juanjo.figue97@gmail.com>
2022-09-20 17:00:52 -0500

    Acceder a el elemento con el nombre de la propiedad

---
+++ Javascript/Basico/accederElemento.js
@@ -0,0 +1,11 @@
+// Configuración
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+const playerNumber = 16;  // Cambia esta línea
+const player = testObj[playerNumber];   // Cambia esta línea
+
+console.log(`El elemento 16 es: ${player}`);
