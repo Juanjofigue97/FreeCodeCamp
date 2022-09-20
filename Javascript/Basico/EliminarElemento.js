f42d1235d6de4c186547522ee26693fd75adb4b3
juanjo.figue97 <juanjo.figue97@gmail.com>
2022-09-20 17:15:44 -0500

    Elminiar un elemento del objeto

---
+++ Javascript/Basico/EliminarElemento.js
@@ -0,0 +1,11 @@
+// Configuración
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+console.log(myDog);
+delete myDog.tails;
+console.log(myDog);
