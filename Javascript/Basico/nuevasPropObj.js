0996f4845a5f8b6d944253f877514791a82c69b0
juanjo.figue97 <juanjo.figue97@gmail.com>
2022-09-20 16:40:38 -0500

    Adicionar un nuevo elemento

---
+++ Javascript/Basico/nuevasPropObj.js
@@ -0,0 +1,15 @@
+// Un objeto
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+console.log(myDog)
+
+// Agregar un objeto
+myDog.bark = 'woof';
+
+console.log("Se Adiciona un elemento");
+console.log(myDog);
+
