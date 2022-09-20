935f0b920fc412ad831e953e39111d8cf656f4a2
juanjo.figue97 <juanjo.figue97@gmail.com>
2022-09-20 17:05:05 -0500

    Actualizar un elemento de un arreglo

---
+++ Javascript/Basico/actualizarElemento.js
@@ -0,0 +1,13 @@
+// Configuración
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+console.log(myDog);
+
+// Actualizacion de elemento
+myDog.name = "Happy Coder";
+
+console.log(myDog);
