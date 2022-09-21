7eb9f72a8961e314be68eb3794bf449c92245458
juanjo.figue97 <juanjo.figue97@gmail.com>
2022-09-21 08:52:24 -0500

    Eliminar un elemento especifico

---
+++ Javascript/EstructuraDeDatosBasicas/buscarElemento.js
@@ -0,0 +1,24 @@
+function filteredArray(arr, elem) {
+  let newArr = [];
+  // Cambia solo el código debajo de esta línea
+  for (let i = 0; i < arr.length; i++) {
+    let add = [...arr[i]];
+    if (add.indexOf(elem) < 0) {
+      newArr.push(add);
+    }
+  }
+  // Cambia solo el código encima de esta línea
+  return newArr;
+}
+
+console.log(
+  filteredArray(
+    [
+      [3, 2, 3],
+      [1, 6, 3],
+      [3, 13, 26],
+      [19, 3, 9],
+    ],
+    3
+  )
+);
